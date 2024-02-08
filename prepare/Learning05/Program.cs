85b886092333fedc8250b47a0bfb7f1ffb2698cc
laurenbrowning <laurenhbs@gmail.com>
2024-02-08 15:21:17 -0700

    w5 Learning activity shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
+
+    
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+
+        Square square1 = new Square("purple", 3);
+        shapes.Add(square1);
+
+        Circle circle1 = new Circle("blue", 10);
+        shapes.Add(circle1);
+
+        Rectangle rec1 = new Rectangle("red", 2, 3);
+        shapes.Add(rec1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
